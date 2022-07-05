9da97240fd22b1efb4794098634f9fd0977b53b7
Sam Miller <sam.dakota@gmail.com>
2022-07-05 13:55:40 -0400

    module 21.3.5

---
+++ client/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';
+
+const Home = () => {
+  // eslint-disable-next-line
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+
+  return (
+    <main>
+      <div className='flex-row justify-space-between'>
+        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
