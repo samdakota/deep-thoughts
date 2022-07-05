5ced80e6dd07703fddf49672820d812eaf28e598
Sam Miller <sam.dakota@gmail.com>
2022-07-05 16:22:26 -0400

    module 21.3.6

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -7,7 +12,7 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const client = new ApolloClient({
@@ -18,9 +23,9 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className='flex-column justify-flex-start min-100-vh'>
+      <div className="flex-column justify-flex-start min-100-vh">
         <Header />
-        <div className='container'>
+        <div className="container">
           <Home />
         </div>
         <Footer />
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS } from '../utils/queries';
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHTS } from "../utils/queries";
+import ThoughtList from "../components/ThoughtList";
 
 const Home = () => {
   // eslint-disable-next-line
@@ -10,8 +11,17 @@ const Home = () => {
 
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">
+          {loading ? (
+            <div>Loading ...</div>
+          ) : (
+            <ThoughtList
+              thoughts={thoughts}
+              title="Some Feed for Thought(s)..."
+            />
+          )}
+        </div>
       </div>
     </main>
   );
